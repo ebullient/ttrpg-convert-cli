f1541e0d146211247d4eacdecacc95d44d0e8932
Erin Schnabel <erinschnabel@gmail.com>
2022-09-06 20:20:14 -0400

    ✨ Add background fluff

--- src/main/java/dev/ebullient/json5e/tools5e/Json2QuteBackground.java
+++ src/main/java/dev/ebullient/json5e/tools5e/Json2QuteBackground.java
@@ -19,10 +19,33 @@ public class Json2QuteBackground extends Json2QuteCommon {
         String backgroundName = decoratedBackgroundName(sources.getName());
         List<String> tags = new ArrayList<>(sources.getSourceTags());
 
+        List<String> text = new ArrayList<>();
+        appendEntryToText(text, node, "##");
+
+        List<String> fluff = getFluff(IndexType.backgroundfluff, "##");
+        if (fluff != null) {
+            boolean found = false;
+            int max = Math.min(text.size(), 10);
+            for (int i = 0; i < max; i++) {
+                if (!found && text.get(i).startsWith("- **")) {
+                    found = true;
+                } else if (found && !text.get(i).startsWith("- **")) {
+                    // first blank line after leading list
+                    text.addAll(i, fluff);
+                    text.add(i, "");
+                    break;
+                }
+            }
+        }
+
+        if (text.isEmpty()) {
+            return null;
+        }
+
         return new QuteBackground(
                 decoratedTypeName(backgroundName, sources),
                 sources.getSourceText(index.srdOnly()),
-                getText("##"),
+                String.join("\n", text),
                 tags);
     }
 
