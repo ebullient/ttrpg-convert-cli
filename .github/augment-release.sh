e2dc9e69a65bee6c3acd9e77cbccb0362c341229
Erin Schnabel <erinschnabel@gmail.com>
2023-05-14 11:31:50 -0400

    🔧 Update release augmentation script

--- .github/augment-release.sh
+++ .github/augment-release.sh
@@ -4,14 +4,17 @@ if [ -z "$1" ]; then
   echo "Specify target version"
   exit 1
 fi
+if [ -z "$JRELEASER_GITHUB_TOKEN" ]; then
+  echo "Specify JRELEASER_GITHUB_TOKEN"
+  exit 1
+fi
 
 export JRELEASER_PROJECT_VERSION=$1
-export JRELEASER_GITHUB_TOKEN=`cat ~/.config/env/gh-cli-token`
 
 git fetch --all
 git checkout ${JRELEASER_PROJECT_VERSION}
 
-mvn clean package -Dnative
+./mvnw clean package -Dnative
 jreleaser assemble -s archive -scp -od target
 jreleaser release -od target -scp -xd uber-jar
 
