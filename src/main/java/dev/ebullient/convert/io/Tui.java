0d4211f88b25d48509530e4bcc868a8a354889d2
Erin Schnabel <erinschnabel@gmail.com>
2023-07-05 19:28:50 -0400

    🔊 capture parse error

--- src/main/java/dev/ebullient/convert/io/Tui.java
+++ src/main/java/dev/ebullient/convert/io/Tui.java
@@ -464,8 +464,8 @@ public class Tui {
     public static JsonNode readTreeFromResource(String resource) {
         try {
             return Tui.MAPPER.readTree(TtrpgConfig.class.getResourceAsStream(resource));
-        } catch (IOException e) {
-            throw new IllegalStateException("Unable to read or parse required resource " + resource, e);
+        } catch (IOException | IllegalArgumentException e) {
+            throw new IllegalStateException("Unable to read or parse required resource (" + resource + "): " + e, e);
         }
     }
 }
