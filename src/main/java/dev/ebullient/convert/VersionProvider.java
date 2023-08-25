065c6b035603577b63c165b712e1857317c5cd30
Erin Schnabel <erinschnabel@gmail.com>
2023-08-25 14:18:15 -0400

    👷 More descriptive snapshot version

--- src/main/java/dev/ebullient/convert/VersionProvider.java
+++ src/main/java/dev/ebullient/convert/VersionProvider.java
@@ -1,16 +1,25 @@
 package dev.ebullient.convert;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import dev.ebullient.convert.config.TtrpgConfig;
 import picocli.CommandLine;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
 
     @Override
     public String[] getVersion() {
-        String version = getClass().getPackage().getImplementationVersion();
-        if (version == null || version.isEmpty()) {
-            version = "dev-snapshot";
+        Properties properties = new Properties();
+        try {
+            properties.load(TtrpgConfig.class.getResourceAsStream("/git.properties"));
+            return new String[] {
+                    "${COMMAND-FULL-NAME} version " + properties.getProperty("git.build.version"),
+                    "Git commit: " + properties.get("git.commit.id.abbrev"),
+                    "Build time: " + properties.get("git.build.time")
+            };
+        } catch (IOException e) {
+            return new String[] { "${COMMAND-FULL-NAME} version unknown " };
         }
-        return new String[] {
-                "${COMMAND-FULL-NAME} version " + version };
     }
 }
