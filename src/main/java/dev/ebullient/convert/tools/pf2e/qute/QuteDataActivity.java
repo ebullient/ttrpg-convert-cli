62d97f2bed59430f7d9ee6026f5d624f29c3e5ac
miscoined <miscoined@gmail.com>
2024-07-06 16:55:44 +1000

    ♻️ Restructure QuteDataActivity
    - Move the enum into QuteDataActivity directly so that we don't have
      to pass the rule, glyph, and name in every time when it'll be staying
      the same for a given action.
    - Make QuteDataActivity store a QuteDataRef to centralize some of the
      link formatting
    - Allows template authors to compare the activity itself and
      theoretically choose what kind of rendering they want

--- src/main/java/dev/ebullient/convert/tools/pf2e/qute/QuteDataActivity.java
+++ src/main/java/dev/ebullient/convert/tools/pf2e/qute/QuteDataActivity.java
@@ -2,7 +2,6 @@ package dev.ebullient.convert.tools.pf2e.qute;
 
 import static dev.ebullient.convert.StringUtil.join;
 
-import dev.ebullient.convert.qute.ImageRef;
 import dev.ebullient.convert.qute.QuteUtil;
 import io.quarkus.qute.TemplateData;
 
@@ -10,25 +9,45 @@ import io.quarkus.qute.TemplateData;
  * Pf2eTools activity attributes. This attribute will render itself as a formatted link:
  *
  * <pre>
- *     [textGlyph](rulesPath "glyph.title")&lt;optional text&gt;
+ *     [textGlyph](rulesPath "action name")&lt;optional text&gt;
  * </pre>
  *
- * @param text The text associated with the action - may be null.
- * @param glyph icon/image representing this activity as a {@link dev.ebullient.convert.qute.ImageRef ImageRef}
- * @param textGlyph A textual representation of the glyph, used as the link text
- * @param rulesPath The path which leads to an explanation of this particular activity
+ * @param activity The type of activity, as a {@link QuteDataActivity.Activity}
+ * @param actionRef A {@link QuteDataRef} to the rules for this particular action type
+ * @param note Text associated with this activity
  */
 @TemplateData
-public record QuteDataActivity(String text, ImageRef glyph, String textGlyph,
-        String rulesPath) implements QuteUtil, QuteDataDuration {
+public record QuteDataActivity(Activity activity, QuteDataRef actionRef, String note) implements QuteUtil, QuteDataDuration {
 
-    /** Return the text associated with the action. */
-    @Override
-    public String text() {
-        return isPresent(text) ? text : glyph.title;
+    public QuteDataActivity(Activity activity, String rulesPath, String note) {
+        this(activity, new QuteDataRef(activity.textGlyph, rulesPath, activity.longName), note);
     }
 
+    /** Return the text associated with the action. */
+    public String text() {
+        return isPresent(note) ? note : activity.longName;
+    }
+
+    @Override
     public String toString() {
-        return join(" ", "[%s](%s \"%s\")".formatted(textGlyph, rulesPath, glyph.title), text);
+        return join(" ", actionRef.toString(), note);
+    }
+
+    public enum Activity {
+        single("Single Action", ">"),
+        two("Two-Action", ">>"),
+        three("Three-Action", ">>>"),
+        free("Free Action", "F"),
+        reaction("Reaction", "R"),
+        varies("Varies", "V"),
+        timed("Duration or Frequency", "⏲");
+
+        public final String longName;
+        public final String textGlyph;
+
+        Activity(String longName, String textGlyph) {
+            this.longName = longName;
+            this.textGlyph = textGlyph;
+        }
     }
 }
