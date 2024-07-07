5144c7b8f08862e04863266eda47100b7c9e3056
miscoined <miscoined@gmail.com>
2024-07-07 23:36:59 +1000

    ♻️ Pull embedded ability construction into Json2QuteAbility
    Generally pull things up into the main class rather than methods of the
    JsonNodeReader.

--- src/main/java/dev/ebullient/convert/tools/pf2e/Pf2eJsonNodeReader.java
+++ src/main/java/dev/ebullient/convert/tools/pf2e/Pf2eJsonNodeReader.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import dev.ebullient.convert.StringUtil;
 import dev.ebullient.convert.tools.JsonNodeReader;
-import dev.ebullient.convert.tools.pf2e.Json2QuteAbility.Pf2eAbility;
 import dev.ebullient.convert.tools.pf2e.Json2QuteAffliction.Pf2eAffliction;
 import dev.ebullient.convert.tools.pf2e.JsonSource.AppendTypeValue;
 import dev.ebullient.convert.tools.pf2e.qute.QuteAbilityOrAffliction;
@@ -134,7 +133,7 @@ public interface Pf2eJsonNodeReader extends JsonNodeReader {
         return streamFrom(source)
                 .map(n -> switch (requireNonNullElse(AppendTypeValue.getBlockType(n), AppendTypeValue.ability)) {
                     case affliction -> (QuteAbilityOrAffliction) Pf2eAffliction.createInlineAffliction(n, convert);
-                    case ability -> Pf2eAbility.createEmbeddedAbility(n, convert);
+                    case ability -> (QuteAbilityOrAffliction) new Json2QuteAbility(convert.index(), n, true).buildQuteNote();
                     default -> {
                         convert.tui().debugf("Unexpected block type in %s", source.toPrettyString());
                         yield null;
