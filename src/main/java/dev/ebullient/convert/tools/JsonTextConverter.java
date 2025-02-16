d86567d29951dd5d539f184060f09fd40fac4d72
Erin Schnabel <erinschnabel@gmail.com>
2025-02-16 16:56:33 -0500

    🐛 Avoid simplifying dice roll for spell level. Resolves #653

--- src/main/java/dev/ebullient/convert/tools/JsonTextConverter.java
+++ src/main/java/dev/ebullient/convert/tools/JsonTextConverter.java
@@ -267,12 +267,17 @@ public interface JsonTextConverter<T extends IndexType> {
                     + " (" + codeString(match.group(3), formulaState) + ")";
         });
 
-        // 7 (`dice:1d6+4|noform|avg` (`1d6 + 4`)) --> `dice:1d6+4|noform|avg|text(7)` (`1d6 + 4`)
-        // 7 (`dice:2d6|noform|avg` (`2d6`)) --> `dice:2d6|noform|avg|text(7)` (`2d6`)
-        text = averageRoll.matcher(text).replaceAll((match) -> {
-            String dice = match.group(2) + dtxt + match.group(1) + ")";
-            return " `" + dice + "` " + match.group(3);
-        });
+        if (text.contains("reach levels")) {
+            // don't look for averages here. This is spell progression
+        } else {
+            // otherwise look for average rolls
+            // 7 (`dice:1d6+4|noform|avg` (`1d6 + 4`)) --> `dice:1d6+4|noform|avg|text(7)` (`1d6 + 4`)
+            // 7 (`dice:2d6|noform|avg` (`2d6`)) --> `dice:2d6|noform|avg|text(7)` (`2d6`)
+            text = averageRoll.matcher(text).replaceAll((match) -> {
+                String dice = match.group(2) + dtxt + match.group(1) + ")";
+                return " `" + dice + "` " + match.group(3);
+            });
+        }
 
         return parseState().inMarkdownTable()
                 ? text.replace("|", "\\|")
